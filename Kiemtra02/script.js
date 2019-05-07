0820643f061e185d2dff06331921452de0fa594e
bv.anh <bv.anh194@gmail.com>
2019-05-07 17:53:05 +0700

    s

---
+++ Kiemtra02/script.js
@@ -0,0 +1,45 @@
+//bài 1
+function timSoLon(a){
+	let max1='';
+	let max2='';
+    
+   for(i= 0; i < a.length; i++) {
+      if( max1 <a[i] ) {
+         max2= max1;
+         max1= a[i];
+      }else if(max2<a[i] ) {
+         max2=a[i];
+      }
+   }
+
+    return max2;
+}
+//bài 3
+function bai3(str,target){
+	var n = str.endsWith("target");
+	return n;
+}
+//bài 2
+function timChuoiDaiNhat(arr){
+  let newarr = [];
+  newarr = arr.sort(function(a, b){return b.length - a.length});
+  return newarr[0];
+
+}
+//bài 4
+function kiemtraLap(arr){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr.length; j++){
+      if(i != j){
+        if(arr[i] == arr[j]){
+          return('true');
+        }
+      }
+    }
+  }
+  return('false');
+}
+//Bài 5
+function checkThe(){
+	document.getElementsByTagName('a2').length;
+}
