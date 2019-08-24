ba3ac8256e2724ecbe6a73af17fca6c65bfcb47c
me <bv.anh194@gmail.com>
2019-08-24 16:27:08 +0700

    testJSON

--- testData/test.js
+++ testData/test.js
@@ -5,12 +5,12 @@ let data=[
     {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000004"},
 ]
 
-let t='';
+let print='';
 for(i=0;i<data.length;i++){
     t+="<tr>"+"<td>"+data[i].name+"</td>"+"<td>"+data[i].year+"</td>"+"<td>"+data[i].email+"</td>"+"<td>"+data[i].sodienthoai+"</td>"+
     "<td><button type='button' class='btn btn-primary btn-sm'>Chỉnh sửa</button><button type='button' class='btn btn-secondary btn-sm'>Xóa</button></td>"+"</tr>";
 }
-document.getElementById("tb").innerHTML=t;
+document.getElementById("tb").innerHTML=print;
 
 
 
