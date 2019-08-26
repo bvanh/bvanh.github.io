9c0df5fa2adaa8c9325c144030786023fac6c776
me <bv.anh194@gmail.com>
2019-08-26 22:41:22 +0700

    testData

--- testData/test.js
+++ testData/test.js
@@ -1,17 +1,31 @@
-let data=[
-    {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000001"},
-    {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000002"},
-    {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000003"},
-    {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000004"},
-]
+// let data=[
+//     {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000001"},
+//     {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000002"},
+//     {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000003"},
+//     {name:"Nguyễn Mạnh Hùng",year:"1990",email:"hung@gmail.com",sodienthoai:"000004"},
+// ]
 
+// let print='';
+// for(i=0;i<data.length;i++){
+//     t+="<tr>"+"<td>"+data[i].name+"</td>"+"<td>"+data[i].year+"</td>"+"<td>"+data[i].email+"</td>"+"<td>"+data[i].sodienthoai+"</td>"+
+//     "<td><button type='button' class='btn btn-primary btn-sm'>Chỉnh sửa</button><button type='button' class='btn btn-secondary btn-sm'>Xóa</button></td>"+"</tr>";
+// }
+// document.getElementById("tb").innerHTML=print;
+
+let request = new XMLHttpRequest();
+request.open('GET', 'file.json', true);
 let print='';
-for(i=0;i<data.length;i++){
-    t+="<tr>"+"<td>"+data[i].name+"</td>"+"<td>"+data[i].year+"</td>"+"<td>"+data[i].email+"</td>"+"<td>"+data[i].sodienthoai+"</td>"+
-    "<td><button type='button' class='btn btn-primary btn-sm'>Chỉnh sửa</button><button type='button' class='btn btn-secondary btn-sm'>Xóa</button></td>"+"</tr>";
+request.onload = function () {
+    // begin accessing JSON data here
+    let data = JSON.parse(this.response);
+    
+    for(i=0;i<data.length;i++){
+        t+="<tr>"+"<td>"+data[i].name+"</td>"+"<td>"+data[i].year+"</td>"+"<td>"+data[i].email+"</td>"+"<td>"+data[i].sodienthoai+"</td>"+
+        "<td><button type='button' class='btn btn-primary btn-sm'>Chỉnh sửa</button><button type='button' class='btn btn-secondary btn-sm'>Xóa</button></td>"+"</tr>";
+    }
+    document.getElementById("tb").innerHTML=print;
 }
-document.getElementById("tb").innerHTML=print;
-
-
+ 
+request.send();
 
     
