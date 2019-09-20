ae02925f483d7ebbeaaf03784f2819344f941ca3
bvanh <48560384+bvanh@users.noreply.github.com>
2019-09-20 10:35:34 +0700

    react-quiz-v1-final-only 1 component

---
+++ react-quiz/precache-manifest.c25d13057781354e05ae21fe286eaec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ce2df4bd5691ae0dbc8ed0ee4781b2",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "abe9b4e0f2c13afc23b7",
+    "url": "/react-quiz/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d51ac08d78fb84eaebd6",
+    "url": "/react-quiz/static/css/main.0d05beb7.chunk.css"
+  },
+  {
+    "revision": "abe9b4e0f2c13afc23b7",
+    "url": "/react-quiz/static/js/2.cae3163c.chunk.js"
+  },
+  {
+    "revision": "d51ac08d78fb84eaebd6",
+    "url": "/react-quiz/static/js/main.4b73895c.chunk.js"
+  },
+  {
+    "revision": "fe23e9de900c2d0a3e56",
+    "url": "/react-quiz/static/js/runtime~main.c5b7b6c1.js"
+  }
+]);
