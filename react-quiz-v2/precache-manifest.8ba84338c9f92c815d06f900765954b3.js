085d3accdc11b643de58d1e2dcc02faaf91be0df
bvanh <48560384+bvanh@users.noreply.github.com>
2019-09-20 16:55:37 +0700

    add react-quiz-v2

---
+++ react-quiz-v2/precache-manifest.8ba84338c9f92c815d06f900765954b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44cd4c2d15f3e0b02d287853e7251d9",
+    "url": "http/index.html"
+  },
+  {
+    "revision": "0894913171f86b926d5e",
+    "url": "http/static/css/2.3a874be4.chunk.css"
+  },
+  {
+    "revision": "a7c3667141f5dc574ad6",
+    "url": "http/static/css/main.a645a6a0.chunk.css"
+  },
+  {
+    "revision": "0894913171f86b926d5e",
+    "url": "http/static/js/2.9105dbec.chunk.js"
+  },
+  {
+    "revision": "a7c3667141f5dc574ad6",
+    "url": "http/static/js/main.be37da66.chunk.js"
+  },
+  {
+    "revision": "2137aa31a993d94deed2",
+    "url": "http/static/js/runtime-main.e684dd59.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "http/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "http/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "http/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "http/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "http/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
