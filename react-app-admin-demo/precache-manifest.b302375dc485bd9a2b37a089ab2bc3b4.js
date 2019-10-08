1828941a137ed86be458aa6d79f6e1ac43135705
bvanh <48560384+bvanh@users.noreply.github.com>
2019-10-08 12:18:30 +0700

    updates

---
+++ react-app-admin-demo/precache-manifest.b302375dc485bd9a2b37a089ab2bc3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f144750302fc521d90892c4b06da8e9c",
+    "url": "/react-app-admin-demo/index.html"
+  },
+  {
+    "revision": "2b852d8af926ffbeac6b",
+    "url": "/react-app-admin-demo/static/css/2.7af79932.chunk.css"
+  },
+  {
+    "revision": "0662caf1918bb1199be4",
+    "url": "/react-app-admin-demo/static/css/main.1bc7fe40.chunk.css"
+  },
+  {
+    "revision": "2b852d8af926ffbeac6b",
+    "url": "/react-app-admin-demo/static/js/2.745c941b.chunk.js"
+  },
+  {
+    "revision": "0662caf1918bb1199be4",
+    "url": "/react-app-admin-demo/static/js/main.965fb1c8.chunk.js"
+  },
+  {
+    "revision": "a5d1f3b20116d7876f7d",
+    "url": "/react-app-admin-demo/static/js/runtime-main.5b840ce0.js"
+  }
+]);
