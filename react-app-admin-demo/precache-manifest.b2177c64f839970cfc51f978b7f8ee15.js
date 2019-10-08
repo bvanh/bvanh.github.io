ea8877f2dd91e5c93b6034f41974ecd5540c8c98
bvanh <48560384+bvanh@users.noreply.github.com>
2019-10-08 12:02:11 +0700

    add app-admin-demo

---
+++ react-app-admin-demo/precache-manifest.b2177c64f839970cfc51f978b7f8ee15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03e98fa9a0a1c604839ec7acdd5c778",
+    "url": "/react-app-addmin-demo/index.html"
+  },
+  {
+    "revision": "990580690b8acb463f1c",
+    "url": "/react-app-addmin-demo/static/css/2.7af79932.chunk.css"
+  },
+  {
+    "revision": "66ee9729cdb2010c0388",
+    "url": "/react-app-addmin-demo/static/css/main.1bc7fe40.chunk.css"
+  },
+  {
+    "revision": "990580690b8acb463f1c",
+    "url": "/react-app-addmin-demo/static/js/2.133f77c8.chunk.js"
+  },
+  {
+    "revision": "66ee9729cdb2010c0388",
+    "url": "/react-app-addmin-demo/static/js/main.d10da9a0.chunk.js"
+  },
+  {
+    "revision": "24fec1c7fd83e9b10af5",
+    "url": "/react-app-addmin-demo/static/js/runtime-main.2af53b6f.js"
+  }
+]);
