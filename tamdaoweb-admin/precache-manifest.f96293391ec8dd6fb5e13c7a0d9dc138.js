ac225d5b0b77b9b440a8b94c6a3fe3c44a6032ad
bvanh <48560384+bvanh@users.noreply.github.com>
2019-11-04 15:50:58 +0700

    tamdaoweb-admin

---
+++ tamdaoweb-admin/precache-manifest.f96293391ec8dd6fb5e13c7a0d9dc138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243c33ef73888e72fac19aedb466d683",
+    "url": "/tamdaoweb-admin/index.html"
+  },
+  {
+    "revision": "15ae235f37650257d87d",
+    "url": "/tamdaoweb-admin/static/css/2.3cab59bc.chunk.css"
+  },
+  {
+    "revision": "04b9b8e566e73790ba52",
+    "url": "/tamdaoweb-admin/static/css/main.a00a1c0f.chunk.css"
+  },
+  {
+    "revision": "15ae235f37650257d87d",
+    "url": "/tamdaoweb-admin/static/js/2.9b1d9482.chunk.js"
+  },
+  {
+    "revision": "04b9b8e566e73790ba52",
+    "url": "/tamdaoweb-admin/static/js/main.0fc69a5d.chunk.js"
+  },
+  {
+    "revision": "7d778eadce6e740cba5a",
+    "url": "/tamdaoweb-admin/static/js/runtime-main.3f583c89.js"
+  }
+]);
